628ecad5058fb51a1e4918d207a75cb9fb63c1c1
jdecheverri47 <josecheverri47@gmail.com>
2024-04-02 10:47:57 -0500

    Add Link components to Header.jsx

--- app/components/ui/Header.jsx
+++ app/components/ui/Header.jsx
@@ -5,6 +5,7 @@ import ButtonWeb from "./ButtonWeb";
 import "@/app/styles/navbar.css";
 
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 function Header() {
   const [scroll, setScroll] = useState(() => {
@@ -16,6 +17,12 @@ function Header() {
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
+    const handleMouseMove = (e) => {
+      const mouseY = e.clientY;
+      if (mouseY <= 200) {
+        setVisible(true);
+      }
+    };
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
       const isVisible = scroll > currentScrollPos;
@@ -24,8 +31,10 @@ function Header() {
       setVisible(isVisible);
     };
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("mousemove", handleMouseMove);
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("mousemove", handleMouseMove);
     };
   }, [scroll]);
 
@@ -41,10 +50,18 @@ function Header() {
           className="w-[3rem] md:w-[4rem]"
         />
         <ul>
-          <li className="li-ben">Benefits</li>
-          <li className="li-ser">Services</li>
-          <li className="li-pri">Pricing</li>
-          <li className="li-ab">About Us</li>
+          <Link href="#Benefits">
+            <li className="li-ben">Benefits</li>
+          </Link>
+          <Link href="#Services">
+            <li className="li-ser">Services</li>
+          </Link>
+          <Link href="#Pricing">
+            <li className="li-pri">Pricing</li>
+          </Link>
+          <Link href="#Faq">
+            <li className="li-ab">FAQ</li>
+          </Link>
         </ul>
         <div>
           <ButtonWeb
