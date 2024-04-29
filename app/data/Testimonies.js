1a55c7abafc91180bf900443b16f17c9cb2e6dd7
jdecheverri47 <josecheverri47@gmail.com>
2024-04-29 09:10:12 -0500

    Update testimonies data with new image URLs

--- app/data/Testimonies.js
+++ app/data/Testimonies.js
@@ -27,7 +27,7 @@ const testimonies = [
     testimony:
       "WebLabs has been instrumental in transforming my concepts into polished websites at a remarkable speed, elevating the quality tenfold.",
     image:
-      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://avatars.githubusercontent.com/u/101738478?v=4",
   },
   {
     id: 4,
